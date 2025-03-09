542f4dd9f5149935a4c9b647e60b6293e0dfb257
th3mein <ugen.thapa@gmail.com>
2025-03-09 12:57:56 +0000

    nodemon config

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,9 @@
+import config from "./config";
+
 export function add(a: number, b: number): number {
+  if (config.debug) {
+    console.debug(`calling add function with arguments ${a} and ${b}`);
+  }
   const obj = {
     hello: "world",
   };
